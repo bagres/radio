630ea96343d52e18a7589adad5c0027df0336661
Fabricio Mauro <fabricio.figueiredo@sefaz.mt.gov.br>
2025-11-14 09:35:41 -0400

    teste alteração da busca de videos por nome.

--- src/main/java/com/example/demo/service/RadioStreamService.java
+++ src/main/java/com/example/demo/service/RadioStreamService.java
@@ -422,7 +422,7 @@ public class RadioStreamService {
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String firstLine = reader.readLine();
 
-            boolean finished = process.waitFor(6, TimeUnit.SECONDS);
+            boolean finished = process.waitFor(10, TimeUnit.SECONDS);
 
             if (!finished || process.exitValue() != 0) {
                 System.err.println("Erro ou Timeout ao executar yt-dlp.");
